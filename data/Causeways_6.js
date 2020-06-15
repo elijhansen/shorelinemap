0e784dbb0cbec2c43eb8bae0ad86ce2b27702fbe
elijhansen <52267024+elijhansen@users.noreply.github.com>
2020-06-15 14:43:21 -0500

    Mobile Layers

--- data/Causeways_6.js
+++ data/Causeways_6.js
@@ -3,9 +3,9 @@ var json_Causeways_6 = {
 "name": "Causeways_6",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "id": null, "Name": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.7925, 42.1474 ], [ -87.79233, 42.14752 ], [ -87.79232, 42.1475 ], [ -87.79248, 42.14738 ], [ -87.7925, 42.1474 ] ] ] ] } },
-{ "type": "Feature", "properties": { "id": null, "Name": "Japanese" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.78572, 42.14671 ], [ -87.78569, 42.14674 ], [ -87.78553, 42.14663 ], [ -87.78557, 42.1466 ], [ -87.78572, 42.14671 ] ] ] ] } },
-{ "type": "Feature", "properties": { "id": null, "Name": "Japanese" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.78582, 42.14774 ], [ -87.78587, 42.14781 ], [ -87.78581, 42.14782 ], [ -87.78576, 42.14776 ], [ -87.78582, 42.14774 ] ] ] ] } },
-{ "type": "Feature", "properties": { "id": null, "Name": "Bird Island Causeway" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.79035, 42.14983 ], [ -87.7902, 42.14987 ], [ -87.79007, 42.14995 ], [ -87.79005, 42.14991 ], [ -87.79016, 42.14983 ], [ -87.79031, 42.14979 ], [ -87.79035, 42.14983 ] ] ] ] } }
+{ "type": "Feature", "properties": { "id": null, "Name": "Main C.way" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.7925, 42.1474 ], [ -87.79233, 42.14752 ], [ -87.79232, 42.1475 ], [ -87.79248, 42.14738 ], [ -87.7925, 42.1474 ] ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "Japanese C.way" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.78571, 42.1467 ], [ -87.78568, 42.14673 ], [ -87.78551, 42.14662 ], [ -87.78555, 42.14658 ], [ -87.78571, 42.1467 ] ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "North Japanese C.way" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.78581, 42.14771 ], [ -87.78588, 42.14782 ], [ -87.78582, 42.14785 ], [ -87.78575, 42.14774 ], [ -87.78581, 42.14771 ] ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "Bird Island C.way" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.79033, 42.14984 ], [ -87.7902, 42.14987 ], [ -87.78998, 42.14998 ], [ -87.78996, 42.14993 ], [ -87.79016, 42.14983 ], [ -87.79029, 42.1498 ], [ -87.79033, 42.14984 ] ] ] ] } }
 ]
 }
