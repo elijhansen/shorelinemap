1478294fd81c952a5cb20a073e7d79afc82d5e94
elijhansen <52267024+elijhansen@users.noreply.github.com>
2020-06-15 16:29:56 -0500

    minor update

---
+++ data/LandFeatures_9.js
@@ -0,0 +1,9 @@
+var json_LandFeatures_9 = {
+"type": "FeatureCollection",
+"name": "LandFeatures_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Name": "Wet Dump" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.78788, 42.14871 ], [ -87.78788, 42.14874 ], [ -87.7878, 42.14874 ], [ -87.7878, 42.14871 ], [ -87.78788, 42.14871 ] ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "Dry Dump" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.78831, 42.14881 ], [ -87.78832, 42.14878 ], [ -87.7882, 42.14872 ], [ -87.78821, 42.14878 ], [ -87.78831, 42.14881 ] ] ] ] } }
+]
+}
