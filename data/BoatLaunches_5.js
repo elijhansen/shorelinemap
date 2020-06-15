f3c27259da3b09903d1223711a838362aac9a74e
elijhansen <52267024+elijhansen@users.noreply.github.com>
2020-06-15 10:33:47 -0500

    First Launch
    Ver 0.1

---
+++ data/BoatLaunches_5.js
@@ -0,0 +1,10 @@
+var json_BoatLaunches_5 = {
+"type": "FeatureCollection",
+"name": "BoatLaunches_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Name": "Japanese Boat Lanch" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.78781, 42.14852 ], [ -87.78789, 42.14863 ], [ -87.78785, 42.14864 ], [ -87.78777, 42.14853 ], [ -87.78781, 42.14852 ] ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "North Lake Boat Lanch" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.78863, 42.14882 ], [ -87.78861, 42.14885 ], [ -87.78858, 42.14884 ], [ -87.78859, 42.14882 ], [ -87.78863, 42.14882 ] ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "Plant Science Boat Launch" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.78615, 42.14389 ], [ -87.78603, 42.14409 ], [ -87.78598, 42.14405 ], [ -87.78608, 42.14384 ], [ -87.78615, 42.14389 ] ] ] ] } }
+]
+}
---
+++ data/Causeways_6.js
@@ -0,0 +1,11 @@
+var json_Causeways_6 = {
+"type": "FeatureCollection",
+"name": "Causeways_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Name": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.7925, 42.1474 ], [ -87.79233, 42.14752 ], [ -87.79232, 42.1475 ], [ -87.79248, 42.14738 ], [ -87.7925, 42.1474 ] ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "Japanese" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.78572, 42.14671 ], [ -87.78569, 42.14674 ], [ -87.78553, 42.14663 ], [ -87.78557, 42.1466 ], [ -87.78572, 42.14671 ] ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "Japanese" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.78582, 42.14774 ], [ -87.78587, 42.14781 ], [ -87.78581, 42.14782 ], [ -87.78576, 42.14776 ], [ -87.78582, 42.14774 ] ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "Bird Island Causeway" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -87.79035, 42.14983 ], [ -87.7902, 42.14987 ], [ -87.79007, 42.14995 ], [ -87.79005, 42.14991 ], [ -87.79016, 42.14983 ], [ -87.79031, 42.14979 ], [ -87.79035, 42.14983 ] ] ] ] } }
+]
+}
